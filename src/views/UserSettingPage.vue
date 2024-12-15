c52ec2c4acf4fffdca32702149d1267df60781a0
Nolonlearner <135989518+Nolonlearner@users.noreply.github.com>
2024-12-16 01:44:31 +0800

    修复了node-sass的错误

--- src/views/UserSettingPage.vue
+++ src/views/UserSettingPage.vue
@@ -108,7 +108,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/styles/mixins.scss';
+@use '@/assets/styles/mixins.scss' as *;
 .settings-page {
   max-width: 100%;
   height: 100%; /* 自适应高度 */
