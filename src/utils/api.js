99d4c9694129db5a666668cc09ccf95a8831c4cf
Nolonlearner <135989518+Nolonlearner@users.noreply.github.com>
2024-12-29 20:05:43 +0800

    完成

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,6 +8,28 @@ const api = axios.create({
   timeout: 1000,
 });
 
+// 请求拦截器
+api.interceptors.request.use(
+  (config) => {
+    // 自动为 POST 请求设置 Content-Type
+    if (config.method === 'post' && !config.headers.get('Content-Type')) {
+      config.headers.set('Content-Type', 'application/json');
+    }
+
+    // 判断是否需要跳过 Authorization 头
+    if (config.headers.get('skipAuth') !== 'true') {
+      const token = Storage.get('token'); // 从本地存储中获取 token
+      config.headers.set('Authorization', `Bearer ${token}`);
+    }
+    // 移除自定义标志以免发送到服务器
+    config.headers.delete('skipAuth');
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error); // 请求错误直接返回
+  }
+);
+
 // 响应拦截器
 api.interceptors.response.use(
   (response) => response, // 正常响应直接返回
@@ -24,6 +46,14 @@ api.interceptors.response.use(
 );
 
 // API 请求方法
-export const login = (payload) => api.post('/user/login', payload);
+export const login = (payload) =>
+  api.post('/user/login', payload, {
+    headers: { skipAuth: true }, // 跳过 Authorization 头
+  });
 
-export const register = (payload) => api.post('/user/register', payload);
+export const register = (payload) =>
+  api.post('/user/register', payload, {
+    headers: { skipAuth: true }, // 跳过 Authorization 头
+  });
+
+export const fetchUserData = () => api.get('/user/data'); // 自动携带 token
